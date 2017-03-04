f16c81de50bc83117331335e188b2b5e02548d66
SnowWalkerJ <jike3212001@163.com>
2017-03-04 23:53:51 +0800

    dist

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='config',
+    name='easy_config',
     version='1.0.0',
     packages=['config'],
-    url='',
+    url='https://github.com/SnowWalkerJ/config',
     license='GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007',
     author='SnowWalkerJ',
     author_email='jike3212001@163.com',
-    description=''
+    description='This package allows you to handle config file easily while allowing you to override settings through commandline params.'
 )
